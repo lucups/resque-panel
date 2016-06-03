461d8df30a08c92c6a15ae8113801830a9632d01
Tony Lu <dev@tony.engineer>
2016-06-03 13:04:15 +0800

    fix phpunit

---
+++ tests/Service/CollectorServiceTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * CollectorServiceTest.php
+ *
+ * @author      Tony Lu <dev@tony.engineer>
+ * @create      16/6/1 22:47
+ * @license     http://www.opensource.org/licenses/mit-license.php
+ */
+
+namespace ResquePanel\Tests\Service;
+
+use ResquePanel\Tests\TestCase;
+
+/**
+ * Class CollectorServiceTest
+ * @package ResquePanel\Tests\Service
+ */
+class CollectorServiceTest extends TestCase
+{
+    public function testPersistCurrentMinuteStatus()
+    {
+    }
+}
